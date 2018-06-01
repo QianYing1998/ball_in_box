ca7245951114602f4d108ee2113ea7914e522ad1
QianYing1998 <39622107+QianYing1998@users.noreply.github.com>
2018-06-01 20:42:10 +0800

    Update ballinbox.py

--- ball_in_box/ballinbox.py
+++ ball_in_box/ballinbox.py
@@ -14,7 +14,7 @@ def ball_in_box(m=5, blockers=[(0.5, 0.5), (0.5, -0.5), (0.5, 0.3)]):
 
     circles = []
 
-    for circle_index in range(m):              #核心算法
+    for circle_index in range(m):              
         tmp1=0
         circles.append((0,0,0))
         for i in range(200):
